05d7db78403211ad1d6f2181ecad0f03afc0d873
Joaquín <84487348+etherlesss@users.noreply.github.com>
2025-05-25 23:20:31 -0400

    Implementacion pantalla principal
    Esta todo en la pantalla principal, y responsive. Faltan solo las recomendaciones de IA.

---
+++ frontend/src/components/cards/HomeCard.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="card">
+        <img :src="props.book.portada" class="card-img-top pt-3 pi-3" :alt="props.book.titulo" />
+        <div class="card-body">
+            <router-link :to="'/book/'+props.book.id_libro" class="subtle-link-styled">
+                <h5 class="card-title">{{ props.book.titulo }}</h5>
+            </router-link>
+            <p class="m-0">{{ props.book.autor }}</p>
+            <p class="m-0">{{ 'Calificación: ' + props.book.promedio + '/10' }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Book } from '@/types/types';
+
+// Definir props
+const props = defineProps<{ book: Book }>();
+
+</script>
+
+<style scoped>
+.card {
+    width: 16rem;
+    height: 22rem;
+    flex: 0 0 auto;
+}
+
+.card img {
+    height: 14rem;
+    object-fit: contain;
+    aspect-ratio: 1/1.6;
+}
+
+.card p {
+    font-size: .871rem;
+    line-height: 135%;
+}
+
+.card h5 {
+    overflow: hidden !important;
+    text-overflow: ellipsis !important;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    line-clamp: 2;
+    -webkit-box-orient: vertical;
+    max-height: 4.5em;
+}
+
+@media (max-width: 576px) {
+    .card {
+        width: 10rem;
+        height: 16rem;
+    }
+
+    .card img {
+        height: 8rem;
+    }
+
+    .card p {
+        font-size: .75rem;
+        line-height: 120%;
+    }
+
+    .card-text {
+        -webkit-line-clamp: 2;
+        line-clamp: 2;
+        max-height: 3.5em;
+    }
+
+    .card h5 {
+        font-size: 1rem;
+    }
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,38 +1,83 @@
-
 <template>
-  <div>
     <Navbar />
-  
-  
-  <div class = "body">
-    <h1>Welcome to the Home Page</h1>
-    <p>This is a simple home page for our application.</p>
-    <div class="test">
-      <img src="../assets/test/Angel4.jpg" alt="foto-prueba">
-    </div>
-  </div>
-
-    <Footer/>
-
-</div>
+    <main class="p-5">
+        <!-- Recomendaciones de IA-->
+        <div v-if="authStore.token">
+            <h2>Sólo para ti</h2>
+        </div>
+        <!-- Mejores calificados -->
+        <div>
+            <h2>Top en calificaciones</h2>
+            <div class="card-scroll">
+                <HomeCard v-for="book in topRating" :key="book.id_libro" :book="book" />
+            </div>
+        </div>
+        <!-- Descubre algo nuevo (aleatorio) -->
+        <div>
+            <h2>Descubre algo nuevo</h2>
+            <div class="card-scroll">
+                <HomeCard v-for="book in discover" :key="book.id_libro" :book="book" />
+            </div>
+        </div>
+    </main>
+    <Footer />
 </template>
+
 <script setup lang="ts">
+import HomeCard from '@/components/cards/HomeCard.vue';
 import Navbar from '@/components/nav/Navbar.vue';
 import Footer from '@/components/pageFooter/Footer.vue';
+import { onMounted, ref } from 'vue';
+import { useAuthStore } from '@/stores/token';
+import { getBooks } from '@/api/api';
+import type { Book } from '@/types/types';
+
+// Definir variables de datos
+const authStore = useAuthStore();
+const books = ref<Book[]>([]);
+const topRating = ref<Book[]>([]);
+const discover = ref<Book[]>([]);
+
+// Obtener 10 libros mejor calificados
+function getTopRatedBooks(books: Book[]) {
+    topRating.value = books.sort((a, b) => b.promedio - a.promedio).slice(0, 10);
+}
+
+// Obtener 10 libros aleatorios
+function getRandomBooks(books: Book[]) {
+    const shuffled = books.sort(() => 0.5 - Math.random());
+    discover.value = shuffled.slice(0, 10);
+}
+
+
+// Obtener los libros desde la API
+async function fetchBooks() {
+    try {
+        const res = await getBooks();
+        if (res.status === 200) {
+            books.value = res.data;
+        }
+    } catch (err) {
+        console.error('Error fetching books:', err);
+    }
+}
+
+// Ejecutar funciones al montar el componente
+onMounted(async() => {
+    await fetchBooks();
+    getTopRatedBooks(books.value);
+    getRandomBooks(books.value);
+});
 </script>
 
 <style scoped>
-
-.test img{
-  width: 100%  ;
-  height: 100% ;
+.card-scroll {
+    display: flex;
+    flex-direction: row;
+    overflow-x: auto;
+    gap: 1rem;
+    padding-bottom: 1rem;
+    scrollbar-width: thin;
+    scrollbar-color: #ccc #f5f5f5;
 }
-
-.body{
-  height: 100%;
-  margin: 0px;
-  min-height: 100vh;
-}
-
-
 </style>
